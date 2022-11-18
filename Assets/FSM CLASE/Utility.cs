24f087d2ff6fe1f65bd75711b6ed2b92c4b63c14
Tatian05 <isp2014asje@gmail.com>
2022-11-17 21:02:54 -0300

    asd2

--- Assets/FSM CLASE/Utility.cs
+++ Assets/FSM CLASE/Utility.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
-//using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System;
+public static class Utility {
 
-//public static class Utility {
-//
-//	public static T Log<T>(T param, string message = "") {
-//		Debug.Log(message +  param.ToString());
-//		return param;
-//	}
-//
-//
-//}
+	public static T Log<T>(T param, string message = "") {
+		Debug.Log(message +  param.ToString());
+		return param;
+	}
+
+	public static int Clampi(int v, int min, int max)
+	{
+		return v < min ? min : (v > max ? max : v);
+	}
+
+	public static IEnumerable<Src> Generate<Src>(Src seed, Func<Src, Src> generator)
+	{
+		while (true)
+		{
+			yield return seed;
+			seed = generator(seed);
+		}
+	}
+}
